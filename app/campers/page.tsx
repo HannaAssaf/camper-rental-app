4a37ed8bf06ee64e22fe44e68b94b6080f23c8b0
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-12-13 23:02:37 +0000

    chore: initialise Next.js project with base layout

---
+++ app/campers/page.tsx
@@ -0,0 +1,7 @@
+export default function CampersPage() {
+  return (
+    <section>
+      <p>Camper list</p>
+    </section>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/styles/variables.css";
+import Header from "@/components/Header/Header";
+
+const inter = Inter({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "Campers Rental App",
+  description:
+    "Discover top-quality campers and travel trucks ready for your next adventure. Browse listings, compare models, and book your ideal travel vehicle.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
