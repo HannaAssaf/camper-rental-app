4619b37efceceabec2669f69e2a32dd6ef11b329
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-12-22 21:11:05 +0000

    feat: add bookingform base

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -1,3 +1,49 @@
+import css from "./BookingForm.module.css";
+
 export default function BookingForm() {
-  return <p>BookingForm</p>;
+  return (
+    <div className={css.card}>
+      <h3 className={css.title}>Book your campervan now</h3>
+      <p className={css.subtitle}>
+        Stay connected! We are always ready to help you.
+      </p>
+
+      <form className={css.form}>
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Name*"
+          className={css.input}
+        />
+
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="Email*"
+          className={css.input}
+        />
+
+        <input
+          type="date"
+          name="date"
+          required
+          placeholder="Booking date*"
+          className={css.input}
+        />
+
+        <textarea
+          name="comment"
+          rows={3}
+          placeholder="Comment"
+          className={css.textarea}
+        />
+
+        <button type="submit" className={css.button}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
