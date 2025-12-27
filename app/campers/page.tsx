3afe9366f3c33d92dd0b5f808a6fd3dfdd244d7b
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-12-27 17:35:56 +0000

    fix: add suspense

--- app/campers/page.tsx
+++ app/campers/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import CampersListClient from "./CamperListClient";
 import css from "./CampersLayout.module.css";
 import Filter from "@/components/Filter/Filter";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Campers | TravelTrucks",
@@ -13,12 +14,14 @@ export default function CampersPage() {
   return (
     <section>
       <div className={css.container}>
-        <aside className={css.sidebar}>
-          <Filter />
-        </aside>
-        <div className={css.content}>
-          <CampersListClient />
-        </div>
+        <Suspense fallback={<div>Loading…</div>}>
+          <aside className={css.sidebar}>
+            <Filter />
+          </aside>
+          <div className={css.content}>
+            <CampersListClient />
+          </div>
+        </Suspense>
       </div>
     </section>
   );
